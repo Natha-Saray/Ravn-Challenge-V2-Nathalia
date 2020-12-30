097587c224e6421234cc8e0081b5be7d78cb4774
Nathalia Saray <56327014+Natha-Saray@users.noreply.github.com>
2020-12-30 08:49:44 -0500

    Creating query to put graphql query, define fields and the data required

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 import { ApolloClient } from '@apollo/client';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider } from '@apollo/client';
+import Characters from './components/characters';
 import './App.css';
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql'
 });
 
+
 function App() {
   return (
     <ApolloProvider client={client}>
     <div className="App">
       <h1>Ravn Star Wars Registry</h1>
+      <Characters />
     </div>
     </ApolloProvider>
+    
   );
 }
+
 
 export default App;
--- client/src/components/characters.js
+++ client/src/components/characters.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import { gql } from 'apollo-boost';
-import { useQuery, ApolloConsumer } from '@apollo/react-hooks';
+// import { gql } from 'apollo-boost';
+import { gql, useQuery } from '@apollo/client';
+// import { useQuery, gql } from '@apollo/client';
+// import { useQuery, ApolloConsumer } from '@apollo/client';
 // import gql from 'graphql-tag';
-// import { Query } from 'react-apollo';
+// import { Query } from '@apollo/client';
+
+const CHARACTERS_QUERY = gql`
+    query CharactersQuery {
+        characters {
+            name
+            eye_color
+            hair_color
+            skin_color
+            birth_year
+        }
+    }
+`;
 
 export class Characters extends Component {
     render() {
         return (
             <div>
-
+                <h1 className="display-4 my-3">Characters</h1>
             </div>
         )
     }
