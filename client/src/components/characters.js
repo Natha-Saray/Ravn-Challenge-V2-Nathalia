adba0f03d0728fb34a77e6eba565e88ab7a8c2b4
Nathalia Saray <56327014+Natha-Saray@users.noreply.github.com>
2020-12-30 09:18:55 -0500

    Imports of gql and query

--- client/src/components/characters.js
+++ client/src/components/characters.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 // import { gql } from 'apollo-boost';
-import { gql, useQuery } from '@apollo/client';
+// import { gql, useQuery } from '@apollo/client';
 // import { useQuery, gql } from '@apollo/client';
 // import { useQuery, ApolloConsumer } from '@apollo/client';
-// import gql from 'graphql-tag';
-// import { Query } from '@apollo/client';
+import gql from 'graphql-tag';
+import { Query } from '@apollo/client';
 
 const CHARACTERS_QUERY = gql`
     query CharactersQuery {
@@ -23,6 +23,15 @@ export class Characters extends Component {
         return (
             <div>
                 <h1 className="display-4 my-3">Characters</h1>
+                <Query query={CHARACTERS_QUERY}>
+                    {({ loading, error, data }) => {
+                        if (loading) return <h4>Loading...</h4>;
+                        if (error) console.log(error);
+                        console.log(data);
+
+                        return <h1>Test</h1>;
+                    }}
+                </Query>
             </div>
         )
     }
