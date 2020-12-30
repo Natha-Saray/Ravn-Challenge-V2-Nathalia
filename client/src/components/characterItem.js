7ccf0c2172414e16f3d92c4838011623b29c6f4b
Nathalia Saray <56327014+Natha-Saray@users.noreply.github.com>
2020-12-30 12:05:05 -0500

    Creating view for characters

--- client/src/components/characterItem.js
+++ client/src/components/characterItem.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-export default function characterItem({ character: { name, eye_color, hair_color, skin_color, birth_yaer } }) {
-    return <div>Test</div>;
+export default function characterItem({ character: { name, eye_color, hair_color, skin_color, birth_yaer } 
+}) {
+    return (
+        <div className="card card-body mb-3">
+            <div className="row">
+                <div className="col-md-9">
+                    <h4>Name: { name }</h4>
+                </div>
+                <div className="col-md-3">
+                    <button className>Chatacter details</button>
+                </div>
+            </div>
+        </div>
+    ); 
 }
