cb2c120486eda8db562082c693b4fffb7614feaa
Nathalia Saray <56327014+Natha-Saray@users.noreply.github.com>
2020-12-30 16:48:30 -0500

    Creating character component and complementing the view for details

--- client/src/components/character.js
+++ client/src/components/character.js
@@ -4,7 +4,7 @@ import { Query } from '@apollo/client';
 import { Link } from 'react-router-dom';
 
 
-const CHARACTERS_QUERY = `gql
+const CHARACTER_QUERY = `gql
     query CharacterQuery(%name: String!) {
         character(name: $name) {
             name
@@ -19,10 +19,47 @@ const CHARACTERS_QUERY = `gql
 export class character extends Component {
     render() {
         return (
-            <div>
-                <h1>Character</h1>    
-            </div>
-        )
+            <fragment>
+                <Query query={CHARACTER_QUERY}>
+                    {({loading, error, data}) => {
+                        if (loading) return <h4>Loading...</h4>;
+                        if (error) console.log(error);
+
+                        const {
+                            name,
+                            eye_color,
+                            hair_color,
+                            skin_color,
+                            birth_year
+                        } = data.character; 
+
+                        return (
+                            <div>
+                                <h1 className="display-4 my-3">
+                                    <span>Name: </span> { name }
+                                </h1>
+                                <h4>Details</h4>
+                                <ul className="list-group">
+                                    <li className="list-group-item">
+                                        Eye color: { eye_color }
+                                    </li>
+                                    <li className="list-group-item">
+                                        Hair color:: { hair_color }
+                                    </li><li className="list-group-item">
+                                        Skin color: { skin_color }
+                                    </li>
+                                    <li className="list-group-item">
+                                        Birth year: { birth_year }
+                                    </li>
+                                </ul>
+                                <hr />
+                                <Link to="/" className="btn btn-secondary">Back</Link>
+                            </div>
+                        );  
+                    }}
+                </Query>
+            </fragment>
+        );
     }
 }
 
