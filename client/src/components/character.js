4cf2241f12fce2c3d3bdc39e1c3adad9f8135ad7
Nathalia Saray <56327014+Natha-Saray@users.noreply.github.com>
2020-12-30 16:07:18 -0500

    Implement and import gql, query and link in character component - Creating character query

--- client/src/components/character.js
+++ client/src/components/character.js
@@ -1,4 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from '@apollo/client';
+import { Link } from 'react-router-dom';
+
+
+const CHARACTERS_QUERY = `gql
+    query CharacterQuery(%name: String!) {
+        character(name: $name) {
+            name
+            eye_color
+            hair_color
+            skin_color
+            birth_year
+        } 
+    }
+`;
 
 export class character extends Component {
     render() {
